9b56408387f411b44e40f1e7a385171d9fc8fa4f
Craig Miller <craigmiller160@gmail.com>
2022-09-18 19:14:26 -0400

    More work on schema

--- src/main/resources/db/migration/V1_01__Base_Schema.sql
+++ src/main/resources/db/migration/V1_01__Base_Schema.sql
@@ -2,9 +2,12 @@ CREATE EXTENSION pg_crypto;
 
 CREATE TABLE positions(
     id UUID NOT NULL,
-    NAME VARCHAR(255),
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
+
+INSERT INTO positions (id, name)
+VALUES (gen_random_uuid(), 'Supervisor'), (gen_random_uuid(), 'Worker');
 
 CREATE TABLE employees(
     id UUID NOT NULL,
@@ -14,4 +17,21 @@ CREATE TABLE employees(
     position_id UUID NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (position_id) REFERENCES positions(id)
+);
+
+CREATE FUNCTION is_supervisor (employee_id UUID) RETURNS BOOLEAN
+AS $$
+SELECT COUNT(*) > 0
+FROM employees
+     JOIN positions ON employees.position_id = positions.id
+WHERE employees.id = employee_id
+  AND positions.name = 'Supervisor'
+$$ LANGUAGE sql;
+
+CREATE TABLE teams(
+    id UUID NOT NULL,
+    supervisor_id UUID NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (supervisor_id) REFERENCES employees (id),
+    CHECK (is_supervisor(supervisor_id))
 );
