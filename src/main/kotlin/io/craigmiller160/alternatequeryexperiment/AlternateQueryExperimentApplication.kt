bc3734a2ccea0f874ed77f08c6b92f69eeb1bb49
Craig Miller <craigmiller160@gmail.com>
2022-09-18 18:34:58 -0400

    Added core dependencies

---
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/AlternateQueryExperimentApplication.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.alternatequeryexperiment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlternateQueryExperimentApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlternateQueryExperimentApplication>(*args)
+}
---
+++ src/test/kotlin/io/craigmiller160/alternatequeryexperiment/AlternateQueryExperimentApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.craigmiller160.alternatequeryexperiment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlternateQueryExperimentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
