f11efaeee9b2aa454db19f60425e7064bcef684d
Craig Miller <craigmiller160@gmail.com>
2022-09-18 19:24:50 -0400

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/alternatequeryexperiment/web/controller/QueryDslControllerTest.kt
@@ -0,0 +1,15 @@
+package io.craigmiller160.alternatequeryexperiment.web.controller
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class QueryDslControllerTest {
+  @Test
+  fun getAllEmployees() {
+    TODO()
+  }
+}
