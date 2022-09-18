ee9e5adf68b4338d8d275d2f3f9bd3c6422d0534
Craig Miller <craigmiller160@gmail.com>
2022-09-18 18:36:58 -0400

    Fixing formatting

--- src/main/kotlin/io/craigmiller160/alternatequeryexperiment/AlternateQueryExperimentApplication.kt
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/AlternateQueryExperimentApplication.kt
@@ -3,9 +3,8 @@ package io.craigmiller160.alternatequeryexperiment
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class AlternateQueryExperimentApplication
+@SpringBootApplication class AlternateQueryExperimentApplication
 
 fun main(args: Array<String>) {
-    runApplication<AlternateQueryExperimentApplication>(*args)
+  runApplication<AlternateQueryExperimentApplication>(*args)
 }
