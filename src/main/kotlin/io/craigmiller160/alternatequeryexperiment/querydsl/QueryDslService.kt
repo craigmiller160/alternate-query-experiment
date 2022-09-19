94f51d69eb6954cfe998eda3e8065d25bd13cc9c
Craig Miller <craigmiller160@gmail.com>
2022-09-19 15:03:19 -0400

    Cleanup

--- src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
@@ -5,14 +5,10 @@ import com.querydsl.jpa.impl.JPAQueryFactory
 import io.craigmiller160.alternatequeryexperiment.data.entity.QEmployee
 import io.craigmiller160.alternatequeryexperiment.data.entity.QPosition
 import io.craigmiller160.alternatequeryexperiment.web.type.EmployeeDTO
-import javax.persistence.EntityManager
 import org.springframework.stereotype.Service
 
 @Service
-class QueryDslService(
-  private val entityManager: EntityManager,
-  private val queryFactory: JPAQueryFactory
-) {
+class QueryDslService(private val queryFactory: JPAQueryFactory) {
   // TODO add pagination
   fun getAllEmployees(): List<EmployeeDTO> {
     // TODO try to find some way to use the constructor of the DTO directly
