ee40f04111262345b337f69d630f98e71ac67614
Craig Miller <craigmiller160@gmail.com>
2022-09-18 20:33:50 -0400

    Progress

--- src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
@@ -1,11 +1,18 @@
 package io.craigmiller160.alternatequeryexperiment.querydsl
 
+import com.querydsl.jpa.impl.JPAQuery
+import io.craigmiller160.alternatequeryexperiment.data.entity.Employee
+import io.craigmiller160.alternatequeryexperiment.data.entity.QEmployee
 import io.craigmiller160.alternatequeryexperiment.web.type.EmployeeDTO
+import javax.persistence.EntityManager
 import org.springframework.stereotype.Service
 
 @Service
-class QueryDslService {
+class QueryDslService(private val entityManager: EntityManager) {
   fun getAllEmployees(): List<EmployeeDTO> {
-    TODO()
+    val query = JPAQuery<Employee>(entityManager)
+    return query.select(QEmployee.employee).from(QEmployee.employee).fetch().map {
+      EmployeeDTO.fromEntities(it)
+    }
   }
 }
--- src/main/kotlin/io/craigmiller160/alternatequeryexperiment/web/type/EmployeeDTO.kt
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/web/type/EmployeeDTO.kt
@@ -1,13 +1,25 @@
 package io.craigmiller160.alternatequeryexperiment.web.type
 
+import io.craigmiller160.alternatequeryexperiment.data.entity.Employee
 import java.time.LocalDate
 import java.util.UUID
 
 data class EmployeeDTO(
   val id: UUID,
-  val firstName: String,
-  val lastName: String,
-  val dateOfBirth: LocalDate,
+  val firstName: String?,
+  val lastName: String?,
+  val dateOfBirth: LocalDate?,
   val positionId: UUID,
   val positionName: String
-)
+) {
+  companion object {
+    fun fromEntities(employee: Employee): EmployeeDTO =
+      EmployeeDTO(
+        id = employee.id,
+        firstName = employee.firstName,
+        lastName = employee.lastName,
+        dateOfBirth = employee.dateOfBirth,
+        positionId = employee.positionId,
+        positionName = "")
+  }
+}
