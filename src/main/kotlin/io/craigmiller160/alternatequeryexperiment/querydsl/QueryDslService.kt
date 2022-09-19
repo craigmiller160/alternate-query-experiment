0edcc4695de0e3e31b707ee0e1978a5bc2b0e192
Craig Miller <craigmiller160@gmail.com>
2022-09-19 14:58:10 -0400

    Improving how I handle querydsl initi

--- src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
+++ src/main/kotlin/io/craigmiller160/alternatequeryexperiment/querydsl/QueryDslService.kt
@@ -1,18 +1,23 @@
 package io.craigmiller160.alternatequeryexperiment.querydsl
 
-import com.querydsl.jpa.impl.JPAQuery
-import io.craigmiller160.alternatequeryexperiment.data.entity.Employee
+import com.querydsl.jpa.impl.JPAQueryFactory
 import io.craigmiller160.alternatequeryexperiment.data.entity.QEmployee
+import io.craigmiller160.alternatequeryexperiment.data.entity.QPosition
 import io.craigmiller160.alternatequeryexperiment.web.type.EmployeeDTO
 import javax.persistence.EntityManager
 import org.springframework.stereotype.Service
 
 @Service
-class QueryDslService(private val entityManager: EntityManager) {
+class QueryDslService(
+  private val entityManager: EntityManager,
+  private val queryFactory: JPAQueryFactory
+) {
+  // TODO add pagination
   fun getAllEmployees(): List<EmployeeDTO> {
-    val query = JPAQuery<Employee>(entityManager)
-    return query.select(QEmployee.employee).from(QEmployee.employee).fetch().map {
-      EmployeeDTO.fromEntities(it)
-    }
+    queryFactory
+      .select(QEmployee.employee, QPosition.position.name)
+      .from(QEmployee.employee, QPosition.position)
+      .where(QPosition.position.id.eq(QEmployee.employee.positionId))
+    TODO()
   }
 }
